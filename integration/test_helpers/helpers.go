4608c82efd5c2c2a348ae4ee991bb9252b874728
Gong Zhang <zhanggbj@cn.ibm.com>
2016-04-26 15:44:14 +0800

    Added bmp login in integration test
    1.Added bmp login in integration test
    2.Added bmp login in bmp status test
    
    [#118158419](https://www.pivotaltracker.com/story/show/118158419)

--- integration/test_helpers/helpers.go
+++ integration/test_helpers/helpers.go
@@ -1,6 +1,8 @@
 package test_helpers
 
 import (
+	"errors"
+	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
@@ -10,6 +12,15 @@ import (
 
 func RunBmp(args ...string) *Session {
 	return RunCommand(BmpExec, args...)
+}
+
+func RunBmpLogin() *Session {
+	Username, Password, err := GetCredential()
+	Expect(err).ToNot(HaveOccurred())
+
+	session := RunBmp("login", "--username", Username, "--password", Password)
+	Expect(session.ExitCode()).To(Equal(0))
+	return session
 }
 
 func RunStemcells(args ...string) *Session {
@@ -24,4 +35,17 @@ func RunCommand(cmd string, args ...string) *Session {
 	session.Wait()
 
 	return session
+}
+
+func GetCredential() (string, string, error) {
+	Username := os.Getenv("BMP_USERNAME")
+	if Username == "" {
+		return "", "", errors.New("BMP_USERNAME environment must be set")
+	}
+
+	Password := os.Getenv("BMP_PASSWORD")
+	if Password == "" {
+		return "", "", errors.New("BMP_PASSWORD environment must be set")
+	}
+	return Username, Password, nil
 }
