37ff48600e27597e113fd44b2e2c091b938d410e
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-31 16:04:47 +0800

    Setup bosh-softlayer-tools pipeline
    1.run UT
    2.run integration test with real bmp server
    3.promote build to s3
    
    [#120351801]

---
+++ ci/tasks/bst-unit.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+base=$( cd "$( dirname "$( dirname "$0" )")"/.. && pwd )
+base_gopath=$( cd $base/../../../.. && pwd )
+export GOPATH=$base/Godeps/_workspace:$base_gopath:$GOPATH
+echo "GOPATH=" $GOPATH
+
+function printStatus {
+      if [ $? -eq 0 ]; then
+          echo -e "\nSWEET SUITE SUCCESS"
+      else
+          echo -e "\nSUITE FAILURE"
+      fi
+  }
+
+trap printStatus EXIT
+
+export BMP_UT_OUTPUT=False
+
+echo -e "\n Cleaning build artifacts..."
+go clean
+
+echo -e "\n Formatting packages..."
+go fmt ./...
+
+echo -e "\n cd to base of project..."
+cd $base
+
+echo -e "\n Unit Testing packages:"
+ginkgo -r -p --noisyPendings --skipPackage=integration
+
+echo -e "\n Vetting packages for potential issues..."
+go tool vet main config cmds common integration
