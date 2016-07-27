4131503e27747a3b6408e06f9c6105af1f05417a
Matt Cui <mattcui@gmail.com>
2016-07-27 14:54:20 +0800

    Added a param in yml

--- ci/tasks/notify-checksum-boshio.sh
+++ ci/tasks/notify-checksum-boshio.sh
@@ -3,16 +3,18 @@
 set -e -x
 
 export BOSHIO_BEARER_TOKEN=$boshio_bearer_token
-export STEMCELL=`echo light-stemcell-final/light*.tgz`
+
+cd light-stemcell-final
+export STEMCELL=`ls light*.tgz`
 
 sha1=`sha1sum ${STEMCELL}`
 sha1s=$(echo $sha1 | tr " " "\n")
 for eachline in $sha1s; do
     if [ -n "${eachline}" ]; then
-    	echo "The checksum (sha1) for $BAT_STEMCELL is ${eachline}, uploading to bosh.io..."
+    	echo "The checksum (sha1) for ${STEMCELL} is ${eachline}, uploading to bosh.io..."
         for((i=1;i<=3;i++));do
         	# curl -X POST 'https://bosh.io/checksums/$BAT_STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer $BOSHIO_BEARER_TOKEN' && exit 0
-        	echo "curl -X POST 'https://bosh.io/checksums/$BAT_STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer $BOSHIO_BEARER_TOKEN'"
+        	echo "curl -X POST 'https://bosh.io/checksums/$STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer ${BOSHIO_BEARER_TOKEN}'"
         done
         echo "Failed to notify bosh.io with checksum ${eachline} after retrying 3 times"
         exit 1
