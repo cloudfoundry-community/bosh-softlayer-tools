c8fc61d075490e6ab58278dd30e66103c39bd4c7
Matt Cui <mattcui@gmail.com>
2016-07-27 21:23:06 +0800

    A better way to check the result

--- ci/tasks/notify-checksum-boshio.sh
+++ ci/tasks/notify-checksum-boshio.sh
@@ -13,7 +13,11 @@ for eachline in $sha1s; do
     if [ -n "${eachline}" ]; then
     	echo "The checksum (sha1) for ${STEMCELL} is ${eachline}, uploading to bosh.io..."
         for((i=1;i<=3;i++));do
-        	curl -X POST "https://bosh.io/checksums/$STEMCELL" -d "sha1=${eachline}" -H "Authorization: bearer ${BOSHIO_BEARER_TOKEN}" && exit 0
+        	res=`curl -X POST "https://bosh.io/checksums/$STEMCELL" -d "sha1=${eachline}" -H "Authorization: bearer ${BOSHIO_BEARER_TOKEN}"`
+        	if [[ "${res}" == *"sha1"* ]]; then
+        	   echo "Notified bosh.io with checksum ${eachline} successfully"
+        	   exit 0
+        	fi
         	sleep 10
         done
         echo "Failed to notify bosh.io with checksum ${eachline} for the stemcell ${STEMCELL} after retrying 3 times"
