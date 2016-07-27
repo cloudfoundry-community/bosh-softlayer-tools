6d7698c214b318fb7fb033c96d864fce818fba90
Matt Cui <mattcui@gmail.com>
2016-07-27 18:01:14 +0800

    Set 10s interval

--- ci/tasks/notify-checksum-boshio.sh
+++ ci/tasks/notify-checksum-boshio.sh
@@ -14,13 +14,14 @@ for eachline in $sha1s; do
     	echo "The checksum (sha1) for ${STEMCELL} is ${eachline}, uploading to bosh.io..."
         for((i=1;i<=3;i++));do
         	# curl -X POST 'https://bosh.io/checksums/$BAT_STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer $BOSHIO_BEARER_TOKEN' && exit 0
-        	echo "curl -X POST 'https://bosh.io/checksums/$STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer ${BOSHIO_BEARER_TOKEN}'"
+        	echo "curl -X POST 'https://bosh.io/checksums/$STEMCELL' -d 'sha1=${eachline}' -H 'Authorization: bearer ${BOSHIO_BEARER_TOKEN}'" && exit 0
+        	sleep 10
         done
-        echo "Failed to notify bosh.io with checksum ${eachline} after retrying 3 times"
+        echo "Failed to notify bosh.io with checksum ${eachline} for the stemcell ${STEMCELL} after retrying 3 times"
         exit 1
     else
-        echo "Fail to generate checksum for the stemcell"
+        echo "Fail to generate checksum for the stemcell ${STEMCELL}"
         exit 1
     fi
-    break
+    exit 1
 done
