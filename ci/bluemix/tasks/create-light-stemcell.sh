58d4177505e63e93ba6e358598a1808bde2b6b28
Zida <shawzida@gmail.com>
2017-08-07 17:25:52 +0800

    Replace building stemcell binary in tasks with s3 resource.

--- ci/bluemix/tasks/create-light-stemcell.sh
+++ ci/bluemix/tasks/create-light-stemcell.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-  set -e
+set -e -x
 
-  # outputs
-  output_dir="light-stemcell"
-  mkdir -p ${output_dir}
+# outputs
+output_dir="light-stemcell"
+mkdir -p ${output_dir}
 
-  echo -e "\n Get stemcell version..."
-  stemcell_version=$(cat version/number | sed 's/\.0$//;s/\.0$//')
+echo -e "\n[INFO] Get stemcell version..."
+stemcell_version=$(cat version/number | sed 's/\.0$//;s/\.0$//')
 
-  base=$( cd "$( dirname "$( dirname "$( dirname "$0" )")")" && pwd )
-  base_gopath=$( cd $base/../../../.. && pwd )
+base=$(pwd)
 
-  export GOPATH=$base_gopath:$GOPATH
+pushd stemcell
+  echo -e "\n Unpacking stemcell raw tgz..."
+  tar -zxvf *.tgz
+  ls -la
+  cp dev_tools_file_list.txt stemcell_dpkg_l.txt "${base}"
+popd
 
-  pushd stemcell
-    echo -e "\n Unpacking stemcell raw tgz..."
-    tar -zxvf *.tgz
-    ls -la
-    cp dev_tools_file_list.txt stemcell_dpkg_l.txt "${base}"
-  popd
+echo -e "\n[INFO] Getting stemcell binary..."
+export sl_stemcells=$(realpath stemcell-cmds/sl_stemcells-*)
 
-  echo -e "\n Creating stemcell binary..."
-  cd "${base}"
-  go build -o out/sl_stemcells main/stemcells/stemcells.go
+echo -e "\n[INFO] Softlayer creating light stemcell..."
+$sl_stemcells -c light-stemcell --version ${stemcell_version} --infrastructure "$IAAS" --stemcell-info-filename "./stemcell-info/stemcell-info.json"
 
-  echo -e "\n Softlayer creating light stemcell..."
-  out/sl_stemcells -c light-stemcell --version ${stemcell_version} --infrastructure "$IAAS" --stemcell-info-filename "${base_gopath}/../stemcell-info/stemcell-info.json"
+echo -e "\n[INFO] Repacking light stemcell with info files..."
+stemcell_filename=`ls light*.tgz`
+tar -zxvf ${stemcell_filename}
+tar -zcvf ${stemcell_filename} image stemcell.MF dev_tools_file_list.txt stemcell_dpkg_l.txt
+cp *.tgz "./${output_dir}/"
 
-  stemcell_filename=`ls light*.tgz`
-  tar -zxvf ${stemcell_filename}
-  tar -zcvf ${stemcell_filename} image stemcell.MF dev_tools_file_list.txt stemcell_dpkg_l.txt
-  cp *.tgz "${base_gopath}/../${output_dir}/"
-
-  checksum="$(sha1sum "${base_gopath}/../${output_dir}/${stemcell_filename}" | awk '{print $1}')"
-  echo "$stemcell_filename sha1=$checksum"
+echo -e "\n[INFO] Printing stemcell sha1 value..."
+checksum="$(sha1sum "./${output_dir}/${stemcell_filename}" | awk '{print $1}')"
+echo "$stemcell_filename sha1=$checksum"
